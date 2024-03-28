ab771c25e31442c1e6c3973c9456747ab9672095
Luiz <luiz_sorrentinoneto@hotmail.com>
2024-03-28 11:10:29 -0300

    3 commit

---
+++ calculadora/src/Comentarios.java
@@ -0,0 +1,30 @@
+// Olá, eu sou um comentário em uma única linha
+
+/* Olá,
+ * Eu sou um comentario
+ * que posso ser mais detalhadod
+ * quando necessário
+ */
+
+ /** 
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
+
+ public class Comentarios {
+    public static void main(String[] args) {
+        
+    
+// não utilizar os comentarios para complementar o entendimento do código 
+    /*
+ * Este método foi elaborado as pressas
+ * por isso eu abrevei o nome das variáveis
+ * mas olha, ele tem a finalidade de somar ou  multiplicar
+ * dois números
+ * */
+   
+    }
+}
